1f95f8b07b1a663f3cfb55988cbed3ca5fddecfd
Quentin <quentin.douris63@gmail.com>
2021-11-11 13:47:12 +0100

    modification du package des classes métiers en entities

--- ServiceChargeAffaire/ServiceChargeAffaire-ejb/src/main/java/miage/m2/entities/ChargerAffaire.java
+++ ServiceChargeAffaire/ServiceChargeAffaire-ejb/src/main/java/miage/m2/entities/ChargerAffaire.java
@@ -2,7 +2,7 @@
  * Projet EAI MenuisMIAGE.
  * Projet réalisé par Quentin DOURIS, Christian MICHIELAN, Trung LE DUC
  */
-package miage.m2.metier;
+package miage.m2.entities;
 
 /**
  * Classe qui représente un objet Charger Affaire
