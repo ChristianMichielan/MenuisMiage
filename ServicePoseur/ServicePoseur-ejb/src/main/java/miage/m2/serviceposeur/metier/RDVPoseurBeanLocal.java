544e7dde056035b0b9df2bfa1b5af9210895602d
Quentin <quentin.douris63@gmail.com>
2021-12-03 08:17:20 +0100

    ajout du bean RDVPoseur

---
+++ ServicePoseur/ServicePoseur-ejb/src/main/java/miage/m2/serviceposeur/metier/RDVPoseurBeanLocal.java
@@ -0,0 +1,26 @@
+/*
+ * Projet EAI MenuisMIAGE.
+ * Projet réalisé par Quentin DOURIS, Christian MICHIELAN, Trung LE DUC
+ */
+package miage.m2.serviceposeur.metier;
+
+import java.util.ArrayList;
+import javax.ejb.Local;
+import miage.m2.exceptions.PoseurConfirmRDVException;
+import miage.m2.serviceposeur.entities.EquipePoseurs;
+import miage.m2.serviceposeur.entities.RDVPoseur;
+
+/**
+ * Interface de l'EJB qui stocke les informations des rendez-vous poseur
+ * @author QuentinDouris
+ */
+@Local
+public interface RDVPoseurBeanLocal {
+    
+    public ArrayList<RDVPoseur> rdvPourUneEquipePoseur(int idEquipe);
+
+    public boolean creerRdvPoseur(String date, int idAffaire, EquipePoseurs equipePoseur, String localisation) throws PoseurConfirmRDVException;
+
+    public boolean equipePoseurDisponible(String date, EquipePoseurs equipePoseur);
+
+}
